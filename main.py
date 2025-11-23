e925cafc507b1cf65854a88ec859c004db79e716
Amelia <amelialou0725@gmail.com>
2025-11-23 15:58:15 -0500

    I did step 1: getting a list containing tuples with each word and it's frequency!

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
-print("hello")
-print("this is a push")
-#hi
+from wordfreq import word_frequency
+
+words_only = []
+
+with open("word_list.txt", "r") as word_list:
+    word_list.readline()
+    for word in word_list:
+        parts = word.strip().split(".")
+        words_only.append(parts[1])    
+
+word_freq_list = []
+for word in words_only:
+    freq = word_frequency(word, 'en', wordlist='best', minimum=0.0)
+    word_freq_list.append((word, freq))
+print(word_freq_list)
