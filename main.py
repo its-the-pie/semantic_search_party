9a31f29c5dc552259f7d02d315f8bdbd72434e22
Amelia <amelialou0725@gmail.com>
2025-12-02 21:34:46 -0500

    Building Visuals

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from wordfreq import word_frequency
 import random 
 import spacy  
-import en_core_web_md
+import en_core_web_lg
+import tkinter as tk
+from tkinter import ttk
 
 def get_freq():
     words_only = []
@@ -15,7 +17,7 @@ def get_freq():
     for word in words_only:
         freq = word_frequency(word, 'en', wordlist='best', minimum=0.0)
         word_freq_list.append((word, freq))
-    return word_freq_list
+    return word_freq_list, words_only
 
 def scale(word_freq_list):
     freqs = []
@@ -42,12 +44,12 @@ def difficulty_score(scaled_freqs):
         scores.append((difficulty, word))
     return scores
 
-def make_bins(scores, num_levels):
+def make_bins(scores, NUM_LEVELS):
     sorted_words = sorted(scores)
-    bin_size = len(sorted_words) // num_levels
+    bin_size = len(sorted_words) // NUM_LEVELS
     bin_dict = {}
-    for i in range(num_levels):
-        if i == num_levels - 1:
+    for i in range(NUM_LEVELS):
+        if i == NUM_LEVELS - 1:
             words_in_bin = sorted_words[i * bin_size:]
         else:
             words_in_bin = sorted_words[i * bin_size : (i + 1) * bin_size]
@@ -66,53 +68,167 @@ def semantic_similarity(doc1, guess, nlp):
     doc2 = nlp(guess)
     num = doc1.similarity(doc2)
     return num
+ 
 
-def play_round(secret_word, nlp):
+def play_game(data, bins, nlp, words_only):
+    while data["level"] <= data["NUM_LEVELS"]:  
+        solved, round_points, round_guesses, round_hints = start_round(data, bins, nlp, words_only)
+        if solved:
+            data["trc"] += 1
+        
+        data["tp"] += data["rp"]
+        data["tg"] += data["rg"]
+        data["th"] += data["rh"]
+        data["level"] += 1  
+    print(f'Game Over!\nScore: {data["tp"]}/ {data["NUM_LEVELS"] * data["MAX_POINTS"]} points\nTotal Rounds Correct: {data["trc"]} / {data["NUM_LEVELS"]}\nTotal Number of Guesses: {data["tg"]}\nTotal Number of Hints: {data["th"]}')
+
+def start_round(data, bins, nlp, words_only):
+    data["sw"] = choose_word(bins, data["level"])
+    print(data["sw"])
+    print(f'Level {data["level"]}')
+
+    data["rp"] = data["MAX_POINTS"]
+    data["rg"] = 0
+    data["rh"] = 0
+    data["already_guessed"] = set()
+
+    result = play_round(data, nlp, words_only)
+    return result
+
+
+def play_round(data, nlp, words_only):
+    round_points = data["MAX_POINTS"]
+    secret_word = data["sw"]
+    round_hints = data["rh"]
+    round_guesses = data["rg"]
     doc1 = nlp(secret_word)
-    score = 0
-    already_guessed = set()
+    max_ss = 0
+    ss_list = []
+
 
     while True: 
         guess = input("Make a guess: ").lower()
-        if guess in already_guessed:
+
+        if guess in data["already_guessed"]:
             print("Already guessed")
         else: 
-            already_guessed.add(guess)
-            score += 1
+            data["already_guessed"].add(guess)
 
             if guess == secret_word.lower():
-                print(f"Correct! Number of guesses: {score}") 
-                return True
+                round_guesses = len(data["already_guessed"])
+                extra = round_guesses - 1
+                round_points -= extra 
+                
+                if round_points < 0:
+                    round_points = 0
+
+                print(f"Correct!\nPoints Earned: {round_points}\nNumber of guesses: {round_guesses}\nNumber of Hints: {round_hints}") 
+                
+                data["rp"] = round_points
+                data["rg"] = round_guesses
+                data["rh"] = round_hints
+                return True, round_points, round_guesses, round_hints
             else:
                 similarity = semantic_similarity(doc1, guess, nlp)
+
                 if similarity > 0.9999 and guess != secret_word.lower():
                     print("Almost there! That was practically a perfect match!")
                 else:        
-                    print(f"Similarity: {similarity:.2f}") 
-                   
-def play_level(level, bins, nlp):
-    secret_word = choose_word(bins, level)
-    print(secret_word)
-    result = play_round(secret_word, nlp)
-    return result 
+                    print(f"Similarity: {similarity:.2f}")
 
-def play_game(num_levels, bins, nlp):
-    level = 1
-    while level <= num_levels:
-        if play_level(level, bins, nlp):
-            level += 1    
+                if similarity > max_ss:
+                    max_ss = similarity
+
+                
+                hint_option = input("Need a hint? (y/n): ").lower()    
+                if hint_option =="y":
+                    if ss_list == []:
+                        for word in words_only: 
+                            ss = semantic_similarity(doc1, word, nlp)
+                            ss_list.append([word, ss])
+
+                    round_hints += 1
+                    round_points -= 2
+
+                    if round_points < 0:
+                        round_points = 0
+                    
+                    upper_bound = max_ss + 0.1 * round_hints
+                    w, ss = hints(max_ss, ss_list, upper_bound)
+                    max_ss = ss
+                
+                forfeit = input("Would you like to forfeit the round? (y/n)").lower()
+                if forfeit == "y":
+                    print(f"Forfeited. The word was: {secret_word}")
+                    round_points = 0
+                    data["rp"] = round_points
+                    data["rg"] = len(data["already_guessed"])
+                    data["rh"] = round_hints
+                    return False, round_points, len(data["already_guessed"]), round_hints
+               
+def hints(max_ss, ss_list, upper_bound): 
+    candidates = [] 
+    for i in ss_list: 
+        word = i[0] 
+        ss = i[1] 
+        if ss > max_ss and ss < upper_bound:
+            candidates.append([word, ss]) 
+            while len(candidates) == 0: 
+                upper_bound += .05 
+                if upper_bound >= 1: 
+                    for i in ss_list: 
+                        if i[1] > max_ss: 
+                            max_word = i[0] 
+                            max_ss = i[1] 
+                            print(f"Closest Word: {max_word}") 
+                            return max_word, max_ss 
+                        for i in ss_list: 
+                            word = i[0] 
+                            ss = i[1]
+                            if ss > max_ss and ss < upper_bound: 
+                                candidates.append([word, ss]) 
+                                hint = random.choice(candidates) 
+                                hint_word = hint[0] 
+                                hint_ss = hint[1] 
+                                print(f"Hint: {hint_word}, Similarity: {hint_ss:.2f}") 
+                                return hint_word, hint_ss
+
+
+
+
+        
+    
+
 
 if __name__ == "__main__":  
-    nlp = en_core_web_md.load()
-    num_levels = 50
-    word_list = get_freq()
-    bins = make_bins(difficulty_score(scale(word_list)), num_levels)
+    nlp = en_core_web_lg.load()
+    NL = 2
+    MP = 100
+    word_freq_list, words_only = get_freq()
+    bins = make_bins(difficulty_score(scale(word_freq_list)), NL)
 
-    play = True
-    while play:
-        play_game(num_levels, bins, nlp)
 
-        restart = input("Would you like to play again? (enter y or n):")
-        if restart != "y":
-            play = False
-            print("Bye! Thanks for playing Semantic Search Party")
+    data = {
+        "NUM_LEVELS": NL,
+        "MAX_POINTS": MP,
+        "level": 1,
+        "tp": 0,
+        "tg": 0,
+        "th": 0,
+        "trc": 0,
+        "rp": 0,
+        "rg": 0,
+        "rh": 0,
+        "already_guessed": set(),
+        "sw": ""
+    }
+
+    # tkinter window
+    root = tk.Tk()
+    root.title("Semantic Search Party") 
+
+
+
+    play_game(data, bins, nlp, words_only)
+
+       
---
+++ main_with_tkinter.py
@@ -0,0 +1,400 @@
+from wordfreq import word_frequency
+import random 
+import spacy  
+import en_core_web_lg
+import tkinter as tk
+from tkmacosx import Button
+
+
+def get_freq():
+    words_only = []
+    with open("cleaned_word_list.txt", "r") as word_list:
+        word_list.readline()
+        for line in word_list:
+            word = line.strip()
+            words_only.append(word)    
+
+    word_freq_list = []
+    for word in words_only:
+        freq = word_frequency(word, 'en', wordlist='best', minimum=0.0)
+        word_freq_list.append((word, freq))
+    return word_freq_list, words_only
+
+def scale(word_freq_list):
+    freqs = []
+    for word, freq in word_freq_list:
+        freqs.append(freq)
+
+    max_freq = max(freqs)
+    min_freq = min(freqs)
+    scaled_freqs = []
+    for word, freq in word_freq_list:
+        scaled_freq = (freq - min_freq) / (max_freq - min_freq)
+        scaled_freqs.append((scaled_freq, word))
+    return scaled_freqs
+
+def difficulty_score(scaled_freqs):
+    max_len = 0
+    for freq, word in scaled_freqs:
+        if len(word) > max_len:
+            max_len = len(word)
+
+    scores = []
+    for freq, word in scaled_freqs:
+        difficulty = (1 - freq) + (len(word) / max_len)
+        scores.append((difficulty, word))
+    return scores
+
+def make_bins(scores, NUM_LEVELS):
+    sorted_words = sorted(scores)
+    bin_size = len(sorted_words) // NUM_LEVELS
+    bin_dict = {}
+    for i in range(NUM_LEVELS):
+        if i == NUM_LEVELS - 1:
+            words_in_bin = sorted_words[i * bin_size:]
+        else:
+            words_in_bin = sorted_words[i * bin_size : (i + 1) * bin_size]
+
+        word_list = []
+        for w in words_in_bin:
+            word_list.append(w[1])
+        bin_dict[i + 1] = word_list
+    return bin_dict
+
+def choose_word(bin_dict, level):
+    word = random.choice(bin_dict[level])
+    return word
+
+def semantic_similarity(doc1, guess, nlp):
+    doc2 = nlp(guess)
+    if doc1.has_vector and doc2.has_vector:
+        return doc1.similarity(doc2)
+    else: 
+        return 0.0
+
+def tk_print(msg, output_box):
+    output_box.configure(state="normal")
+    output_box.insert(tk.END, msg + "\n")
+    output_box.see(tk.END)
+    output_box.configure(state="disabled")
+
+def start_round(data, nlp, output_box, frame):
+    if "restart_button" in data and data["restart_button"]:
+        data["restart_button"].destroy()
+        data["restart_button"] = None
+
+    output_box.configure(state="normal")
+    output_box.delete("1.0", tk.END)
+    output_box.insert("1.0", "SEMANTIC SEARCH PARTY\n")
+    output_box.tag_add("title", "1.0", "2.0")
+    output_box.tag_configure("title",font=("Times New Roman", 30, "bold"), foreground="white")
+    output_box.insert(tk.END, "\n")
+    output_box.delete("3.0", tk.END)
+    output_box.configure(state="disabled")
+
+    
+    if data["level"] > data["NUM_LEVELS"]:
+        if data["last_lev"]:
+            tk_print(f'Forfeited. The word was: {data["sw"]}', output_box)
+        tk_print("Congratulations! You've completed all the levels!", output_box)
+        tk_print(f'Total Points: {data["tp"]} / {data["MAX_POINTS"] * data["NUM_LEVELS"]}', output_box)
+        tk_print(f'Total Guesses: {data["tg"]}', output_box)
+        tk_print(f'Total Hints Used: {data["th"]}', output_box)
+        tk_print("Wanna play again?", output_box)
+
+        restart_button = Button(frame, text="Restart Game", bg="green", fg="white", font=("Times New Roman", 14), command=lambda: restart(data, nlp, output_box, frame))
+        restart_button.grid(row=3, column=0, columnspan=4, sticky="nsew", pady=5, padx=5)
+        return
+    
+    data["sw"] = choose_word(data["bins"], data["level"])
+    data["rp"] = data["MAX_POINTS"]
+    data["rg"] = 0
+    data["rh"] = 0
+    data["already_guessed"] = set()
+    data["doc1"] = nlp(data["sw"])
+    data["max_ss"] = 0
+    data["ss_list"] = []
+    data["num_guess"] = 0
+    data["hints_given"] = set()
+    data["letters_given"] = 0
+    data["num_guess"] = 0
+    data["restart_button"] = None
+    data["last_lev"] = False
+
+
+    for word in data["words_only"]:
+        ss = semantic_similarity(data["doc1"], word, nlp)
+        data["ss_list"].append([word, ss])        
+
+    tk_print("-" * 67, output_box)
+    tk_print(f'Level {data["level"]}', output_box)
+    tk_print("Start Guessing!!", output_box)
+    tk_print("-" * 67, output_box)
+    box(output_box)
+
+
+def play_round(guess, data, nlp, output_box):
+    guess = guess.lower()
+
+    if guess in data["already_guessed"]:
+        tk_print("Already guessed", output_box)
+        return
+    else: 
+        data["already_guessed"].add(guess)
+        secret_word = data["sw"]
+        doc1 = data["doc1"]
+        max_ss = data["max_ss"]
+        ss_list = data["ss_list"]
+        round_hints = data["rh"]
+
+        if guess == secret_word.lower():
+            round_guesses = len(data["already_guessed"])
+            round_points = data["rp"] 
+                
+            if round_points < 0:
+                round_points = 0
+
+            add_guess(data, output_box, guess, 1.0)
+            tk_print("Correct!", output_box)
+            tk_print(f"Points Earned: {round_points} / {data['MAX_POINTS']}", output_box)
+            tk_print(f"Number of guesses: {round_guesses}", output_box) 
+            tk_print(f"Number of Hints: {round_hints}", output_box) 
+            data["rp"] = round_points
+            data["rg"] = round_guesses
+            data["tp"] += data["rp"]
+            data["tg"] += data["rg"]
+            data["th"] += data["rh"]
+            data["trc"] += 1
+            data["level"] += 1
+
+            return True, round_points, round_guesses, round_hints
+        else:
+            similarity = semantic_similarity(doc1, guess, nlp)
+            if similarity < 0:
+                similarity = 0
+                
+            if similarity > 0.9999 and guess != secret_word.lower():
+                tk_print("Almost there! That was practically a perfect match!", output_box)
+            else:        
+                add_guess(data, output_box, guess, similarity)
+
+            if similarity > max_ss:
+                data["max_ss"] = similarity
+
+def restart(data, nlp, output_box, frame):
+    data["level"] = 1
+    data["tp"] = 0
+    data["tg"] = 0
+    data["th"] = 0
+    data["trc"] = 0
+    start_round(data, nlp, output_box, frame)
+
+def hints(data, output_box):
+    upper_bound = data["max_ss"] + 0.2 * data["rh"]
+        
+    candidates = [] 
+    for word, ss in data["ss_list"]: 
+        if word != data["sw"] and ss > data["max_ss"] and ss < upper_bound and word not in data["hints_given"]:
+            candidates.append([word, ss]) 
+
+    while len(candidates) == 0: 
+        upper_bound += 0.05 
+        if upper_bound >= 1: 
+            for i in data["ss_list"]: 
+                if ss > data["max_ss"]: 
+                    max_word = word 
+                    data["max_ss"] = ss 
+                    tk_print(f"Closest Word: {max_word}", output_box) 
+                    return 
+                else: 
+                    tk_print("No more hints available", output_box)
+                    return
+                        
+        else:
+            for word, ss in data["ss_list"]: 
+                if ss > data["max_ss"] + 0.1 and ss < upper_bound: 
+                    candidates.append([word, ss]) 
+                                
+    hint = random.choice(candidates) 
+    hint_word = hint[0] 
+    hint_ss = hint[1] 
+    data["hints_given"].add(hint_word)
+    data["max_ss"] = hint_ss
+    data["rh"] += 1
+    data["rp"] -= 2
+    if data["rp"] < 0:
+        data["rp"] = 0
+
+    tk_print(f"Hint:\n", output_box)
+    add_guess(data, output_box, hint_word, hint_ss)
+
+def letter_reveal(data, output_box): 
+    given = data["letters_given"]
+
+    if given >= len(data["sw"]) - 2:
+        tk_print("You've used up all the letter reveal hints for this round", output_box)
+
+    else:
+        given += 1
+        data["letters_given"] = given
+        letters = data["sw"][:given]
+        remain = len(data["sw"]) - given
+        data["rp"] -= 10
+        if data["rp"] < 0:
+            data["rp"] = 0
+        tk_print(f'Letter Reveal: {letters + "_ " * remain}', output_box)
+
+def on_guess(data, nlp, output_box, guess_entry):
+    guess = guess_entry.get().strip()
+    if not guess or guess == "Enter a word...":
+        return
+    
+    guess_entry.delete(0, tk.END)   
+    result = play_round(guess, data, nlp, output_box) 
+    
+    if result and result[0] is True: 
+        output_box.after(2000, lambda: start_round(data, nlp, output_box, frame))
+
+
+
+def on_hint(data, nlp, output_box):
+    hints(data, output_box)
+
+def on_forfeit(data, nlp, output_box):
+    if data["level"] == data["NUM_LEVELS"]:
+        data["last_lev"] = True
+    else:
+        tk_print(f'Forfeited. The word was: {data["sw"]}', output_box)
+        data["last_lev"] = False
+
+    data["rp"] = 0
+    data["rg"] = len(data["already_guessed"])
+    data["level"] += 1
+    output_box.after(100, lambda: start_round(data, nlp, output_box, frame))
+
+
+def box(output_box):
+    output_box.configure(state="normal")
+    topic = "#\tGuess\tSimilarity\n"
+    output_box.insert("end", topic)
+    output_box.insert("end", "-" * 67 + "\n")
+    output_box.configure(state="disabled")
+
+def add_guess(data, output_box, word, ss):
+    data["num_guess"] += 1
+    num_guess = data["num_guess"]
+    output_box.configure(state="normal")
+
+    if ss < 0.33:
+        color = "red"
+    elif ss < 0.66:
+        color = "orange"
+    else:
+        color = "green"
+    if not output_box.tag_names().__contains__(color):
+        output_box.tag_configure(color, foreground=color)
+
+    row_txt = f"{num_guess}\t{word}\t{ss:.2f}\n"
+    output_box.insert("end", row_txt, color)
+
+    output_box.tag_configure(color, foreground=color)
+    output_box.configure(state="disabled")
+    output_box.see("end")
+
+def temp_msg(entry, msg):
+    entry.insert(0, msg)
+    entry.configure(fg="grey")
+
+    def on_click(event):
+        if entry.get() == msg:
+            entry.delete(0, tk.END)
+            entry.configure(fg="black")
+    def click_out(event):
+        if entry.get() == "":
+            entry.insert(0, msg)
+            entry.configure(fg="grey")
+    
+    entry.bind("<FocusIn>", on_click)
+    entry.bind("<FocusOut>", click_out)
+
+
+if __name__ == "__main__":  
+    nlp = en_core_web_lg.load()
+    NL = 3
+    MP = 100
+    word_freq_list, words_only = get_freq()
+    bins = make_bins(difficulty_score(scale(word_freq_list)), NL)
+
+    data = {
+        "NUM_LEVELS": NL,
+        "MAX_POINTS": MP,
+        "level": 1,
+        "tp": 0,
+        "tg": 0,
+        "th": 0,
+        "trc": 0,
+        "rp": 0,
+        "rg": 0,
+        "rh": 0,
+        "already_guessed": set(),
+        "sw": "",
+        "doc1": 0, 
+        "max_ss": 0,
+        "ss_list": [],
+        "bins": bins, 
+        "words_only": words_only,
+        "hints_given": set(), 
+        "letters_given": 0, 
+        "num_guess": 0, 
+        "restart_button": None, 
+        "last_lev": False
+    }
+    
+    # tkinter window
+    root = tk.Tk()
+    root.title("Semantic Search Party") 
+    root.configure(bg="navy")
+    
+    frame = tk.Frame(root, bg="navy")
+    frame.grid(row=0, column=0, sticky="nsew", padx=10, pady=10)
+
+    root.columnconfigure(0, weight=1)
+    root.rowconfigure(0, weight=1)
+    
+    for i in range(4):
+        frame.columnconfigure(i, weight=1)
+    frame.rowconfigure(0, weight=1)
+
+    output_box = tk.Text(frame, width=50, height=15, bg="black", fg="white", font=("Times New Roman", 18), selectbackground="purple", selectforeground="white")
+    output_box.grid(row=0, column=0, columnspan=4, sticky="nsew")
+    output_box.configure(state="disabled")
+    output_box.configure(tabs=("60p", "250p"))
+
+
+    guess_entry = tk.Entry(frame,bg="white", fg="black", font=("Times New Roman", 18), insertbackground="black")
+    guess_entry.grid(row=1, column=0, columnspan=2, sticky="ew", pady=(0,5))
+    guess_entry.bind("<Return>", lambda e: on_guess(data, nlp, output_box, guess_entry))
+    temp_msg(guess_entry, "Enter a word...")
+
+    guess_button = Button(frame, text="Guess", bg="blue", fg="white", font=("Times New Roman", 18), activebackground="darkblue", activeforeground="white", highlightbackground="blue", highlightthickness=1, command=lambda: on_guess(data, nlp, output_box, guess_entry))
+    guess_button.grid(row=1, column=2, columnspan=2, sticky="ew", pady=(0,5))
+    
+
+    hint_button = Button(frame, text="Hint (-2)", bg="purple", fg="white", font=("Times New Roman", 14), activebackground="indigo", activeforeground="white", highlightbackground="purple", highlightthickness=1,command=lambda: on_hint(data, nlp, output_box), width=10)
+    hint_button.grid(row=2, column=0, sticky="ew", padx=5, pady=5)
+
+    forfeit_button = Button(frame, text="Forfeit Round", width=10, bg="orange", fg="white", font=("Times New Roman", 14), activebackground="darkorange", activeforeground="white", highlightbackground="orange", highlightthickness=1,command=lambda: on_forfeit(data, nlp, output_box))
+    forfeit_button.grid(row=2, column=2, sticky="ew", padx=5, pady=5) 
+
+    quit_button = Button(frame, text="Quit", bg="red", fg="white", font=("Times New Roman", 14), activebackground="darkred", activeforeground="white", highlightbackground="red", highlightthickness=1,command=root.destroy, width=10)
+    quit_button.grid(row=2, column=3, sticky="ew", padx=5, pady=5) 
+
+    reveal_button = Button(frame, text="Reveal Letter (-5)", bg="green", fg="white", font=("Times New Roman", 14), activebackground="darkgreen", activeforeground="white", highlightbackground="green", highlightthickness=1, command=lambda: letter_reveal(data, output_box), width=10)
+    reveal_button.grid(row=2, column=1, sticky="ew", padx=5, pady=5)
+
+    start_round(data, nlp, output_box, frame)
+    root.mainloop()
+
+    
+
+       
